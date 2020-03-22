b2cc79358b12a94aa68eb07c08dffe1906d6c08b
kSefo <tdutnrij@gmail.com>
2020-03-22 15:20:41 +0900

    Modified table definition

--- sql/01-create-tables.sql
+++ sql/01-create-tables.sql
@@ -1,11 +1,44 @@
+DROP TABLE IF EXISTS `sales_day`;
+CREATE TABLE IF not exists `sales_day` (
+  `sales_day` VARCHAR(10) NOT NULL  COMMENT '営業日',
+  `start_time` VARCHAR(10) NOT NULL COMMENT '開始時間',
+  `end_time` VARCHAR(10) NOT NULL   COMMENT '終了時間',
+  `holiday` TINYINT(1) NOT NULL     COMMENT '休業日',
+  `created` DATETIME NOT NULL       COMMENT '作成時間',
+  `updated` DATETIME NOT NULL       COMMENT '更新時間',
+  PRIMARY KEY (`sales_day`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF not exists `user` (
+  `user_id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL COMMENT 'ユーザID',
+  `user_name` VARCHAR(20) NOT NULL                   COMMENT '氏名',
+  `user_tel` VARCHAR(20) NOT NULL                    COMMENT '電話番号',
+  `user_email` VARCHAR(50) NOT NULL                  COMMENT 'メールアドレス',
+  `created` DATETIME NOT NULL                        COMMENT '作成時間',
+  `updated` DATETIME NOT NULL                        COMMENT '更新時間',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE IF not exists `menu` (
+  `menu_id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL COMMENT 'メニューID',
+  `menu_name` VARCHAR(20) NOT NULL                   COMMENT 'メニュー名',
+  `created` DATETIME NOT NULL                        COMMENT '作成時間',
+  `updated` DATETIME NOT NULL                        COMMENT '更新時間',
+  PRIMARY KEY (`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
 DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE IF not exists `reservation` (
-  `id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL COMMENT 'ID',
-  `reservation_datetime` DATETIME NOT NULL      COMMENT '予約日時',
-  `reservation_name` VARCHAR(20) NOT NULL       COMMENT '予約者名',
-  `request` TEXT NOT NULL                       COMMENT '要望・相談',
-  `status`  VARCHAR(10) NOT NULL                COMMENT 'ステータス',
-  `created` DATETIME NOT NULL                   COMMENT '作成時間',
-  `updated` DATETIME NOT NULL                   COMMENT '更新時間',
-  PRIMARY KEY (`id`)
+  `reservation_id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '予約ID',
+  `user_id` INT(10) NOT NULL                                COMMENT 'ユーザID',
+  `menu_id` INT(10) NOT NULL                                COMMENT 'メニューID',
+  `reservation_datetime` DATETIME NOT NULL                  COMMENT '予約日時',
+  `created` DATETIME NOT NULL                               COMMENT '作成時間',
+  `updated` DATETIME NOT NULL                               COMMENT '更新時間',
+  PRIMARY KEY (`reservation_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
